4b2ab0d77d8719e96523f11ad9a9613b6c55ce64
Emil <Emil@EMIL-DESKTOP>
2018-02-04 10:54:51 +0100

    Second commit, because it didn't commit all files from last commit

--- Game/Game/main.cpp
+++ Game/Game/main.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
 
-int main()
+#include <SDL.h>
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, char *argv[])
 {
-	std::cout << "Hello!" << std::endl;
-	std::cin.ignore();
-	return 0;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+	SDL_DisplayMode DM;
+	SDL_GetCurrentDisplayMode(0, &DM);
+
+	SDL_Window *window = SDL_CreateWindow(
+		"Orange Horizon",
+		DM.w / 2 - WIDTH / 2,
+		DM.h / 2 - HEIGHT / 2,
+		WIDTH,
+		HEIGHT,
+		SDL_WINDOW_OPENGL);
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to init GLEW" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	SDL_Event windowEvent;
+
+	while (true)
+	{
+		if (SDL_PollEvent(&windowEvent))
+		{
+			if (windowEvent.type == SDL_QUIT)
+			{
+				break;
+			}
+		}
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
 }
